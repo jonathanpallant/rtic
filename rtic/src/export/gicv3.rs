701c14927ab21e0bd22548a341807116694ee9f1
Jonathan Pallant <jonathan.pallant@ferrous-systems.com>
2025-09-04 18:20:00 +0100

    Need arm-gic patches.

--- rtic/src/export/gicv3.rs
+++ rtic/src/export/gicv3.rs
@@ -1,3 +1,5 @@
+use arm_gic::gicv3::GicV3;
+
 pub mod interrupt {
     #[inline]
     pub fn disable() {
@@ -12,4 +14,42 @@ pub mod interrupt {
 
 pub fn pend() {
     
+}
+
+pub fn run<F>(priority: u8, f: F)
+where
+    F: FnOnce(),
+{
+    if priority == 1 {
+        // If the priority of this interrupt is `1` then PMR can only be `0`
+        f();
+        GicV3::set_priority_mask(0);
+    } else {
+        let initial = GicV3::get_priority_mask();
+        f();
+        unsafe { GicV3::set_priority_mask(initial) }
+    }
+}
+
+/// Lock implementation using GICv3 Priority Mask Register (PMR)
+///
+/// # Safety
+///
+/// The system ceiling is raised from current to ceiling
+/// by raising the PMR to the ceiling value, or
+#[inline(always)]
+pub unsafe fn lock<T, R>(
+    ptr: *mut T,
+    ceiling: u8,
+    f: impl FnOnce(&mut T) -> R,
+) -> R {
+    let current = GicV3::get_priority_mask();
+    // Only interrupts with a higher priority (numerically lower) will be signalled.
+    // Priorities in RTIC follow a higher value = more important scheme, so invert.
+    GicV3::set_priority_mask(u8::MAX - ceiling);
+    let r = unsafe {
+        f(&mut *ptr)
+    };
+    GicV3::set_priority_mask(current);
+    r
 }
